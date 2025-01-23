4277cf8d048bcde727ff773a3ae362475a506774
farrukhsrdya <frahmatullayev55@gmail.com>
2025-01-24 01:53:54 +0500

    first commit

--- e_shop/index/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-
-
-urlpatterns = [
-    path('', views.home_page),
-    path('product/<int:pk>', views.product_page),
-    path('category/<int:pk>', views.category_page)
-]
